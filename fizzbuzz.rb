bcf37701dbf75c6e7c56c66305552ca3da9a7017
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-12-21 18:50:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,3 @@
-def fizzbuzz(n)
-    if n / 15 == 0
-      return fizzbuzz
-    elsif n / 5 == 0
-      return buzz
-    elsif n / 3 == 0
-      return fizz
-    else
-      nil
-end
+def fizzbuzz
+
+end 
